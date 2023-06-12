bb0ba0d7cde66963648f2f425e525fd8b45f3f49
Israr khan <khanisrar2397@gmail.com>
2023-06-12 18:49:14 +0530

    add

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -12,8 +12,7 @@ const Login = ({ onLogin }) => {
 
     try {
       setIsLoading(true);
-
-      const response = await fetch(`https://project1-pi-three.vercel.app/api/user/login`, {
+      const response = await fetch(`http://localhost:3000/api/user/login`, {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: {
